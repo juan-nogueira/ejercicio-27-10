ebb8cbf3a1f6ebbdb9e2e632a9bb34bb0bef88e4
Juan Marsilli <juanmarsilli147@gmail.com>
2025-10-27 08:57:44 -0300

    Schemas creados

---
+++ backend/models/Category.js
@@ -0,0 +1,21 @@
+// Category Schema 
+const categorySchema = new mongoose.Schema({ 
+  name: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    trim: true 
+  }, 
+  description: String, 
+  parentCategory: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Category', 
+    default: null 
+  }, 
+  isActive: { 
+    type: Boolean, 
+    default: true 
+  } 
+});
+
+module.exports = mongoose.model('Category', categorySchema);
