4b428897796822c58de14bf539ec54ee44b2a0a3
Juan Marsilli <juanmarsilli147@gmail.com>
2025-10-30 09:20:25 -0300

    Buscar Productos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom"
 import SearchCategories from './pages/SearchCategories'
+import SearchProducts from './pages/SearchProducts'
 function App() {
   return (
     <>
       <Routes>
         <Route path="/search-categories" element={<SearchCategories />} />
+        <Route path="/search-products" element={<SearchProducts />} />
       </Routes>
     </>
   )
