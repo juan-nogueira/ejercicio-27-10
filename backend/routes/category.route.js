409895f625e66859ecfe60813db6c889d3b14f79
Matías Moreno <matifmorenoxd@gmail.com>
2025-10-30 09:30:43 -0300

    added middleare to routs

--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/category.controller');
+import { logger } from "../middlewares/logger.js";
 
 
-router.post('/', categoryController.createCategory); // Crear categoría
-router.get('/', categoryController.getAllCategories);// Listar categorías
-router.get('/:id', categoryController.getCategoryById); // Obtener categoría por ID
-router.put('/:id', categoryController.updateCategory); // Actualizar categoría
-router.delete('/:id', categoryController.deleteCategory); // Eliminar categoría
+router.post('/', logger, categoryController.createCategory); // Crear categoría
+router.get('/', logger, categoryController.getAllCategories);// Listar categorías
+router.get('/:id', logger, categoryController.getCategoryById); // Obtener categoría por ID
+router.put('/:id', logger, categoryController.updateCategory); // Actualizar categoría
+router.delete('/:id', logger, categoryController.deleteCategory); // Eliminar categoría
 
 module.exports = router;
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/product.controller');
+import { logger } from "../middlewares/logger.js";
 
 
-router.post('/', productController.createProduct);// Crear producto
-router.get('/', productController.getAllProducts);// Listar productos
-router.get('/:id', productController.getProductById);// Obtener producto por ID
-router.put('/:id', productController.updateProduct);// Actualizar producto
-router.delete('/:id', productController.deleteProduct);// Eliminar producto
+router.post('/', logger, productController.createProduct);// Crear producto
+router.get('/', logger, productController.getAllProducts);// Listar productos
+router.get('/:id', logger, productController.getProductById);// Obtener producto por ID
+router.put('/:id', logger, productController.updateProduct);// Actualizar producto
+router.delete('/:id', logger, productController.deleteProduct);// Eliminar producto
 
 module.exports = router;
